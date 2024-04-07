1cc499b32210fce64ea5de20966a9ea8e8cd65c9
Drake224025 <sasithamare@gmail.com>
2024-04-07 17:23:07 +0530

    feat: Create PUT method for Books

--- backend/index.js
+++ backend/index.js
@@ -67,6 +67,35 @@ app.get("/books/:id", async (request, response) => {
   }
 });
 
+// Route to Update a Book
+app.put("/books/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndUpdate(id, request.body);
+    console.log("result", result);
+
+    if (!result) {
+      return response.status(404).json({ message: "Book not found" });
+    }
+
+    return response.status(200).send({ message: "Book updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(mongoDbURL)
   .then(() => {
